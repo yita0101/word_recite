b73ee9ed01dc3d66f84ed8ce841fb173f825ca59
Arrake <arrake@163.com>
2021-04-01 20:08:27 +0800

    单词网站第一次从猜想到实现，date：2020.3.09， 一、完成的功能有： 1、可以在任意位置双击，然后出现输入框。。。 2、输入框可以通过失去焦点，和enter转化为单词span 3、单词span可以通过双击重新出现输入框，但enter或失去焦点后出现错误 二、实现的技术： 1、技术栈：纯js，和css布局 2、在任意位置点击出现输入框,在没有学定位之前有点突破常规，也就是没有见过，没有这种概念性的想法， 突破想法的还是，对position和鼠标事件的理解，知道它们可以做什么，比如：发生双击事件，我就可以明确的获取它们相对于父元素的位置 三、bug 1、enter或失去焦点后出现错误

---
+++ test.js
@@ -0,0 +1,87 @@
+window.onload=function(){
+    let rihgt=document.querySelector(".right");
+
+    let input;
+
+    rihgt.ondblclick=function(event){
+        event=event||window.event;
+        let x=event.offsetX
+        let y=event.offsetY
+
+        input=document.createElement("input");
+
+        input.style.position="absolute";
+        input.type="text";
+
+        input.style.left=x+"px";
+        input.style.top=y+"px";
+
+        rihgt.appendChild(input);
+
+        input.focus();
+
+        input.onblur=function(){
+            save()
+
+        }
+
+        input.onkeydown=function(event){
+
+            if(event.keyCode==13){
+                save()
+            }
+
+
+
+
+        }
+
+    }
+
+
+    function save(){
+
+        let span=document.createElement("span");
+
+        if(!/^\s*$/.test(input.value)){
+            span.innerText=input.value
+            span.style.position="absolute";
+
+            span.style.left=input.style.left;
+            span.style.top=input.style.top;
+            span.style.backgroundColor="red";
+
+            input.parentNode.replaceChild(span,input);
+
+            span.ondblclick=function(event){
+                event=event||window.event;
+                let newInput=document.createElement("input")
+                newInput.type="text";
+                newInput.style.position="absolute";
+                newInput.style.left=span.style.left;
+                newInput.style.top=span.style.top;
+                newInput.value=span.innerText;
+                newInput.onblur=function(){save()
+                }
+                newInput.onkeydown=function(event){
+
+                    if(event.keyCode==13){
+                        save()
+                    }
+
+                }
+
+                span.parentNode.replaceChild(newInput,span)
+
+                newInput.focus()
+
+                window.event? window.event.cancelBubble = true : e.stopPropagation();
+            }
+
+
+        }else{
+            input.parentNode.removeChild(input);
+        }
+    }
+
+}
